7d3ec555b1d9b2986e228b5e4a77ca467c92a047
WG <wg5329@163.com>
2017-08-17 14:17:08 +0800

    新增截图

--- separatededittext/src/main/java/com/kenny/separatededittext/SeparatedEditText.java
+++ separatededittext/src/main/java/com/kenny/separatededittext/SeparatedEditText.java
@@ -9,7 +9,6 @@ import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.text.InputFilter;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
